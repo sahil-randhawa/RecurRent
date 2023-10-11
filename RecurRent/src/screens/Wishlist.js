105c3c5b21c5bf9435b7b6977545feb15f824604
shagun-thukral19 <123033995+shagun-thukral19@users.noreply.github.com>
2023-10-11 01:37:46 -0400

    FavList product names displayed in wishlist, remove feature pending

--- RecurRent/src/screens/Wishlist.js
+++ RecurRent/src/screens/Wishlist.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, FlatList, TextInput } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, ActivityIndicator } from "react-native";
 import {
     primaryColor,
     secondaryColor,
@@ -12,15 +12,125 @@ import {
     darkTheme,
     formStyles,
 } from "../styles/GlobalStyles";
-import Btn, { primaryBtnStyle } from "../components/Button";
+import Icon from 'react-native-vector-icons/Entypo';
 import { auth, db } from "../../firebaseConfig";
-import { collection, addDoc, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where, doc, getDoc } from "firebase/firestore";
 
 const Wishlist = () => {
+    const [user, setUser] = useState({});
+    const [wishList, setWishList] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchFromDB();
+    }, []);
+
+    useEffect(() => {
+		if (user.favlist) {
+		  getFavList();
+		}
+	  }, [user]);
+
+    const fetchFromDB = async () => {
+        try {
+            const q = query(collection(db, "userProfiles"), where("email", "==", auth.currentUser.email));
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                setUser(doc.data());
+            });
+        } catch (error) {
+            console.log(error);
+        } 
+    };
+
+    const getFavList = async () => {
+		const resultsFromFirestore = [];
+                await Promise.all(
+                    user.favlist.map(async (value) => {
+                        try {
+                            const docRef = doc(db, 'Products', value);
+                            const docSnap = await getDoc(docRef);
+
+                            if (docSnap.exists()) {
+                                const data = docSnap.data();
+                                console.log('Document data:', data);
+                                resultsFromFirestore.push(data);
+                            } else {
+                                console.log('Document does not exist.');
+                            }
+                        } catch (error) {
+                            console.error('Error fetching document:', error);
+                        }
+                    })
+                );
+                setWishList(resultsFromFirestore);
+                console.log("WishList data:", wishList);
+                
+                try { }
+                finally {
+                    setLoading(false);
+                }
+	}
+
+    const handlePress = (item) => {
+        // Handle press on the item, e.g., navigate to details or perform an action
+        console.log(`Pressed item: ${item}`);
+    };
+
     return (
         <View style={spacing.container}>
-            <Text>Wishlist here</Text>
+            {loading ? (
+                <ActivityIndicator size="large" color={primaryColor} />
+            ) : wishList.length === 0 ? (
+                <Text style={{ textAlign: "center", alignSelf: "center" }}>Currently, your wishlist is empty.{'\n'}Keep Browsing!</Text>
+            ) : (
+                <FlatList
+                    data={wishList}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity style={styles.itemContainer} onPress={() => handlePress(item)}>
+                            <View style={styles.itemContent}>
+                                <Text style={styles.itemText}>{item.name}</Text>
+                            </View>
+                            <Icon name="chevron-right" size={24} color={textColor.primary} style={styles.arrowIcon} />
+                        </TouchableOpacity>
+                    )}
+                    keyExtractor={(item) => item.name}
+                />
+            )}
         </View>
-    )
-}
-export default Wishlist
+    );
+};
+
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+    itemContainer: {
+        width: 380,
+        //this should be changed so that it automatically fits the screen
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: lightTheme.colors.border,
+        backgroundColor: lightTheme.colors.primaryContainer,
+    },
+    itemContent: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    itemText: {
+        fontSize: 16,
+        color: textColor.primary,
+    },
+    arrowIcon: {
+        marginLeft: 10,
+    },
+});
+
+export default Wishlist;
