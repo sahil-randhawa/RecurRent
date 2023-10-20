0fb5c83982c95651b43248f92ad9cec82d93165a
Sahil Randhawa <sahildeep.randhawa@gmail.com>
2023-10-20 01:10:22 -0400

    firebase auth asyncstorage

--- RecurRent/firebaseConfig.js
+++ RecurRent/firebaseConfig.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,6 +21,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
-const auth = getAuth(app)
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 
-export {db, auth}
+export { db, auth }
