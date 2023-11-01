b425ca3d1a769acd2d90b93718c9f967efbb3e4b
Chitra Raval <chitraval56@gmail.com>
2023-11-01 19:29:42 -0400

    start working on chat from renter side!

--- RecurRent/src/screens/ProfileTabs/Messages.js
+++ RecurRent/src/screens/ProfileTabs/Messages.js
@@ -39,7 +39,53 @@ import {
 	documentId,
 } from "firebase/firestore";
 
-const Messages = ({ navigation }) => {
+const Messages = ({ navigation,route }) => {
+
+	const [userRequestListings, setUserRequestListings] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		getUserListings();
+	}, []);
+
+
+	const getUserListings = async () => {
+		try {
+			const q = query(collection(db, "Bookings"), where("renterID", "==", auth.currentUser.uid),where("bookingStatus", "==" , "Requested"));
+			const querySnapshot = await getDocs(q);
+	  
+			const allUserRequestListings = [];
+			querySnapshot.forEach(async (docr) => {
+				docData = docr.data()
+				console.log("Product Id:", docData.productID)
+				try {
+					const productDocRef = doc(db, "Products", docData.productID);
+					const productDoc = await getDoc(productDocRef);
+			
+					if (productDoc.exists()) {
+					  const productData = productDoc.data();
+					  console.log("Document data:", productData);
+					  allUserRequestListings.push(productData);
+					} else {
+					  console.log("No such document for product ID:", docData.productID);
+					}
+				  } catch (error) {
+					console.error("Error getting product document:", error);
+				  }
+				 
+			});
+	  
+			setUserRequestListings(allUserRequestListings); // Set the state with the array of documents
+		  } catch (error) {
+			console.error("Error fetching data from Firestore:", error);
+		}
+
+		try {
+		} finally {
+			setLoading(false);
+		}
+	};
+
 	return (
 		<>
 			<View style={spacing.container}>
