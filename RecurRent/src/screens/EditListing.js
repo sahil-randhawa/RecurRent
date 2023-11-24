9db1a99a9a53586d9bc8c6d12e6746d90ed7cdb3
sahil-randhawa <sahildeep.randhawa@gmail.com>
2023-11-24 16:51:18 -0500

    android picker fix, image edit update, signout button scroll view fix

--- RecurRent/src/screens/EditListing.js
+++ RecurRent/src/screens/EditListing.js
@@ -4,6 +4,8 @@ import {
 	Text,
 	ScrollView,
 	StyleSheet,
+	TouchableOpacity,
+	Image,
 } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
 import {
@@ -20,33 +22,104 @@ import {
 } from "../styles/GlobalStyles";
 import Input from "../components/Input";
 import Btn, { primaryBtnStyle } from "../components/Button";
-import { auth, db } from "../../firebaseConfig";
-import { setDoc, doc } from "firebase/firestore";
+import { auth, db, firebase } from "../../firebaseConfig";
+import { collection, addDoc, setDoc, doc, updateDoc } from "firebase/firestore";
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+import Toast from 'react-native-toast-message';
+import * as Location from "expo-location";
 
 const EditListing = ({ navigation, route }) => {
-	const [name, setName] = useState("StudyTable");
+	const [name, setName] = useState("");
 	const [description, setDescription] = useState(
-		"Good condition study table from IKEA. Color: Black."
+		""
 	);
-	const [price, setPrice] = useState("30");
+	const [price, setPrice] = useState("");
 	const [pickUpAddress, setpickUpAddress] = useState(
-		"1 Younge Street, Toronto, Canada"
+		""
 	);
 	const [duration, setDuration] = useState("");
 	const [category, setCategory] = useState("");
-	const [image, setImage] = useState("table");
+	const [imageToUpload, setImageToUpload] = useState("table");
 
 	useEffect(() => {
 		// Update state variables with values from route.params when the screen loads
-        const selectedProduct = route.params.combinedData.selectedProduct;
-        setName(selectedProduct.item.name);
-        setDescription(selectedProduct.item.description);
-        setPrice(selectedProduct.item.price);
-        setpickUpAddress(selectedProduct.item.pickUpAddress);
+		const selectedProduct = route.params.combinedData.selectedProduct;
+		console.log('selectedProduct Id: ', selectedProduct.item.productId);
+		setName(selectedProduct.item.name);
+		setDescription(selectedProduct.item.description);
+		setPrice(selectedProduct.item.price);
+		setpickUpAddress(selectedProduct.item.pickUpAddress);
 		setDuration(selectedProduct.item.duration);
 		setCategory(selectedProduct.item.category);
-		setImage(selectedProduct.item.name); //Need to change
+		setImageToUpload(selectedProduct.item.productPhoto); //Need to change
 	}, []);
+
+	const pickImage = async () => {
+		console.log('Picking image...');
+		try {
+			// setImageToUpload(null);
+			const result = await ImagePicker.launchImageLibraryAsync({
+				// launchCameraAsync
+				mediaTypes: ImagePicker.MediaTypeOptions.Images, // All, Images, Videos
+				// mediaTypes: ImagePicker.MediaTypeOptions.All,
+				allowsEditing: true,
+				aspect: [4, 3],
+				quality: 1,
+			});
+			// console.log('Selected result :' + JSON.stringify(result));
+			if (!result.canceled) {
+				// setImage(result.uri);
+				setImageToUpload(result.assets[0].uri);
+				console.log('Image selected!' + JSON.stringify(result, null, 2));
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
+	const uploadImage = async (productDocId) => {
+		try {
+			// delete the old image from firebase storage - skip for now
+
+			// upload the new image to firebase storage
+			const { uri } = await FileSystem.getInfoAsync(imageToUpload);
+			const blob = await new Promise((resolve, reject) => {
+				const xhr = new XMLHttpRequest();
+				xhr.onload = function () {
+					resolve(xhr.response);
+				};
+				xhr.onerror = function (e) {
+					console.log(e);
+					reject(new TypeError('Network request failed'));
+				};
+				xhr.responseType = 'blob';
+				xhr.open('GET', uri, true);
+				xhr.send(null);
+			});
+
+			const filename = imageToUpload.substring(
+				imageToUpload.lastIndexOf('/') + 1
+			);
+
+			// const ref = firebase.storage().ref().child(uuid.v4());
+			const ref = firebase.storage().ref().child(filename);
+
+			const snapshot = await ref.put(blob);
+
+			blob.close();
+			const url = await snapshot.ref.getDownloadURL();
+			console.log('Successfully uploaded! Image url : ', url);
+
+			// update the image url for the new document
+			const productDocRef = doc(db, "Products", productDocId);
+			await updateDoc(productDocRef, { productPhoto: url });
+			console.log('Successfully updated product image url!\n url: ', url);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
 
 	//Route Data
 	const [selectedProduct, setSelectedProduct] = useState(
@@ -55,28 +128,40 @@ const EditListing = ({ navigation, route }) => {
 
 	const updateButtonHandler = async () => {
 		const docRef = doc(db, "Products", selectedProduct.item.productId);
-	  
+
 		const updatedData = {
-		  name: name,
-		  description: description,
-		  price: price,
-		  pickUpAddress: pickUpAddress,
-		  duration: duration,
-		  category: category,
-		  productPhoto: "https://source.unsplash.com/600x500/?" + image,
+			name: name,
+			description: description,
+			price: price,
+			pickUpAddress: pickUpAddress,
+			duration: duration,
+			category: category,
+			productPhoto: imageToUpload,
 		};
-	  
+
 		try {
-		  await setDoc(docRef, updatedData, { merge: true });
-		  console.log("Document updated successfully");
-		  alert("Listing updated successfully!");
-		  navigation.navigate("Listings");
+			await setDoc(docRef, updatedData, { merge: true });
+			console.log("Document updated successfully");
+			alert("Listing updated successfully!");
+			if (imageToUpload) {
+				uploadImage(selectedProduct.item.productId);
+			}
+			Toast.show({
+				type: 'success',
+				position: 'bottom',
+				text1: 'Listing updated successfully!',
+				visibilityTime: 3000,
+				autoHide: true,
+				topOffset: 30,
+				bottomOffset: 40,
+			});
+			navigation.navigate("Listings");
 		} catch (error) {
-		  console.error("Error updating document: ", error);
+			console.error("Error updating document: ", error);
 		}
-	  };
-	  
-	  
+	};
+
+
 
 	return (
 		<>
@@ -192,7 +277,7 @@ const EditListing = ({ navigation, route }) => {
 						/>
 					</View>
 
-					<View style={formStyles.fieldContainer}>
+					{/* <View style={formStyles.fieldContainer}>
 						<Text style={formStyles.label}>Image</Text>
 						<Input
 							placeholder="eg.Fan"
@@ -200,6 +285,71 @@ const EditListing = ({ navigation, route }) => {
 							value={image} //Need to change
 							style={formStyles.input}
 						/>
+						</View> */}
+
+					<View style={formStyles.fieldContainer}>
+						<Text style={formStyles.label}>Image</Text>
+						<TouchableOpacity
+							// style={formStyles.input}
+							onPress={pickImage}
+						>
+							{!imageToUpload && (
+								<Text
+									style={{
+										marginVertical: 8,
+										backgroundColor: "#fff",
+										padding: 10,
+										fontSize: 16,
+										borderRadius: 5,
+										height: 40,
+										borderColor: primaryColor,
+										borderWidth: 1,
+										color: "#9E9E9E",
+									}}
+								>
+									{imageToUpload ? imageToUpload : "Select item image"}
+								</Text>
+							)}
+							{imageToUpload && (
+								<View style={{ position: 'relative' }}>
+									<Image
+										source={{ uri: imageToUpload }}
+										style={{
+											width: 80,
+											height: 80,
+											marginVertical: 8,
+										}}
+									/>
+									{/* discard image button X */}
+									<TouchableOpacity
+										onPress={() => setImageToUpload(null)}
+										style={{
+											position: 'absolute',
+											top: -5,
+											left: 65,
+											borderRadius: 50,
+											backgroundColor: 'rgba(91, 129, 250, 0.8)',
+											paddingHorizontal: 8,
+											paddingVertical: 5,
+										}}
+									>
+										<Text style={{
+											color: 'white',
+											fontSize: 16,
+											fontWeight: 'bold'
+										}}>X</Text>
+									</TouchableOpacity>
+								</View>
+							)}
+						</TouchableOpacity>
+
+
+
+						{/* <TextInput
+							style={formStyles.input}
+							onChangeText={(text) => setImage(text)}
+							value={image}
+						/> */}
 					</View>
 
 					<Btn
